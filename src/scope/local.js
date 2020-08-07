0373eded978f703520279dc5e6830b93c3f01d93
Jesús David Cuéllar <jedave.co@gmail.com>
2020-08-07 19:54:31 -0400

    Scope Local

---
+++ src/scope/local.js
@@ -0,0 +1,22 @@
+const Hello = () => {
+    const HelloWorld = 'Hello World';
+    console.log(HelloWorld);
+}
+Hello();
+console.log(HelloWorld)//No se puede acceder a la variable
+
+// Segundo Ejemplo.
+
+var scope = 'Hello I am Global';
+
+const HelloPrint = () =>{
+    var scope = 'I am Local';
+    //La variable se toma como local para la función func
+    //Esto se le conoce cómo ámbito léxico
+    const func = () =>{
+        return scope
+    }
+    console.log(func());
+    console.log(scope)
+}
+HelloPrint();
